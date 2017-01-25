ef77634a4ef391ec61186a051aee0e4713488327
Bollie <music@ca9.eu>
2017-01-25 21:37:12 +0100

    Various changes.

--- ControllerScripts/YamahaMOXF/Yamaha-MOXF.control.js
+++ ControllerScripts/YamahaMOXF/Yamaha-MOXF.control.js
@@ -1,7 +1,8 @@
 loadAPI(1);
 
 // Controller properties
-host.defineController("Yamaha", "MOXF6/8", "1.0", "48d487d0-df04-11e6-9598-0800200c9a66");
+host.defineController("Yamaha", "MOXF6/MOXF8", "1.0", 
+    "48d487d0-df04-11e6-9598-0800200c9a66");
 
 host.defineMidiPorts(5,5);
 host.addDeviceNameBasedDiscoveryPair(
@@ -26,12 +27,18 @@ host.addDeviceNameBasedDiscoveryPair(
 function init() {
     // Map callbacks
     host.getMidiInPort(1).setMidiCallback(onMidiCtrl);
+    host.getMidiInPort(4).setSysexCallback(onMidiSysEx);
+
+    out5 = host.getMidiOutPor(4);
 
     // Map some stuff
     transport = host.createTransportSection();
     application = host.createApplication();
     cursorTrack = host.createCursorTrack(0, 0);
     primaryDevice = cursorTrack.createCursorDevice()
+
+    out5.sendSysex("F0 43 10 7F 17 01 20 00 00 F7");
+    sendTrackName("BitWig");
 
 }
 
@@ -52,6 +59,19 @@ function onMidiCtrl(status, data1, data2) {
             cursorTrack.selectFirst();
             application.getAction("show_insert_popup_browser").invoke();
         }
+    }
+}
+
+/** Receives sysex messages from input 5
+*/
+function onMidiSysEx(data) {
+    printMidi(data);
+}
+
+function sendTrackName( name ) {
+    for ( var i=0; i<15; i++) {
+        var c = i<name.length ? name.charCodeAt(i) : 0x20;
+        out5.sendSysex("F0 43 10 7F 17 01 00 "+hexpad(i)+" "+hexpad(c)+" F7")
     }
 }
 
